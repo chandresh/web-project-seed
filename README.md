ff3d6ee696e7b83a577d7b63e8eb1d429857cd13
Chandresh Pant <cp@cpant.in>
2019-03-24 20:14:55 +0530

    Initial Commit with basic setup done
      - Webpack
      - ts loader
      - css and scss loader
      - file-loader for images
      - HTML template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Web Project Seed
+
+This is a basic seed project that I mostly use for trainings. It uses webpack.
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader" // translates CSS into CommonJS
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    port: 3000
+  }
+};
